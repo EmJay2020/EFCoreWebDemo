0f928ef5eb411559af11bd8f16f48c84bca0e43c
Avrumi Friedman <a.friedman07@gmail.com>
2020-04-27 22:10:06 -0400

    Add project files.

---
+++ EFCoreWebDemo.Data/PeopleContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebDemo.Data
+{
+    public class PeopleContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public PeopleContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
